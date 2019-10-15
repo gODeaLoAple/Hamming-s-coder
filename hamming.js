7278b0d648bc02308b308a6a5eaac54262720fcb
gODeaLoAple <maksim.tebaykin.01@mail.ru>
2019-10-15 22:40:28 +0500

    1) Some cosmetic changes (using functional methods) 2) Use a set but not an array in the decode method 3) Change error-corrector (replaceData)

--- hamming.js
+++ hamming.js
@@ -1,66 +1,61 @@
 function invert(data, index) {
     return data.substr(0, index)
-        + (data[index] == "0" ? "1" : "0")
+        + (data[index] === "0" ? "1" : "0")
         + data.substr(index + 1);
 }
 
-function replaceData(data, errorPosition) {
-    let result = data;
-
-    switch (errorPosition.length) {
-        case 0: 
-            break;
-        case 1: 
-            result = invert(data, errorPosition[0]);
-            break;
-        default: 
-            result = invert(data, errorPosition[0]) + "1";
-            break;
-    }
-
+function getNumbersSet(from, to) {
+    let result = new Set();
+    for (let i = from; i <= to; i++)
+        result.add(i);
     return result;
 }
 
+function replaceData(data, errorPositions) {
+    switch (errorPositions.length) {
+        case 0: 
+            return data;
+        case 1: 
+            return invert(data, errorPositions[0]);
+        case 3: 
+            if (errorPositions[1] > 3)
+                return invert(data, errorPositions[0]);
+        case 7:
+            return invert(data, 3);
+        default: 
+            return invert(data, errorPositions[0]) + "1";
+    }
+}
+
 function encodeHamming(data) {
-    let positions = [ [ 1, 2, 3 ], [ 0, 2, 3 ], [ 0, 1, 3 ] ];
+    const positions = [ [ 1, 2, 3 ], [ 0, 2, 3 ], [ 0, 1, 3 ] ];
     let evenCode = ""; 
 
-    for (let i = 0; i < positions.length; i++) {
+    positions.forEach( (round) => {
         let isEven = true;
-
-        for (let j = 0; j < positions[i].length; j++) {
-            if (data[positions[i][j]] == "1") 
+        round.forEach( (position) => {
+            if (data[position] === "1")
                 isEven = !isEven;
-        }
-
+        });
         evenCode += isEven ? "0" : "1";
-    }
+    });
 
     return data + evenCode;
 }
 
 function decodeHamming(data) {
-    let positions = [ [ 1, 2, 3, 4 ], [ 0, 2, 3, 5 ], [ 0, 1, 3, 6 ] ];
-    let errors = [];
-    let errorPosition = [];
+    const positions = [ [ 1, 2, 3, 4 ], [ 0, 2, 3, 5 ], [ 0, 1, 3, 6 ] ];
+    let hasError = getNumbersSet(0, 6);
 
-    positions.forEach( (position) => {
+    positions.forEach( (round) => {
         let isEven = true;
-        
-        position.forEach( (index) => {
-            if (data[index] == "1")
+        round.forEach( (index) => {
+            if (data[index] === "1")
                 isEven = !isEven;
         });
-
-        position.forEach( (index) => {
-            errors[index] = isEven || errors[index];
-        });
-    });
-
-    errors.forEach( (err, i) => {
-        if (!err) 
-            errorPosition.push(i);
+        if (isEven)
+            round.forEach( (index) => hasError.delete(index) );
     });
     
-    return replaceData(data, errorPosition); 
+    return replaceData(data, Array.from(hasError).sort()); 
 }
